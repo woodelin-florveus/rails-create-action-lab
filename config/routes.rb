5b7259f000d519ef135678be522830f545d2a807
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-24 22:54:52 -0500

    commit tree e6cd13a3eed1c90c117b0a65019e24d1f0360c17

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :new, :create]
-  get '/student/:id', to: 'students#show', as: 'student'
+  # resources :students, only: [:index, :new, :create]
+
+  resources :posts, only: [:index, :show, :new, :create]
+
+  # get '/students', to: 'students#index', as: 'students'
+
+  
+  # get '/students/new', to: 'students#new', as: 'new_student'
+  
+  # post '/students', to: 'students#create'
+  
+  # get '/students/:id', to: 'students#show', as: 'student'
 end
